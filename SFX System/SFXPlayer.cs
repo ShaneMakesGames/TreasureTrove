e37a29fe9622e6728a6c3e495154fbed1988dd09
Shane Tupler <61483325+ShaneMakesGames@users.noreply.github.com>
2022-02-20 11:39:35 -0500

    Add files via upload

--- SFX System/SFXPlayer.cs
+++ SFX System/SFXPlayer.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum SoundState
+{
+    Playing,
+    Paused,
+    Finished
+}
+
 public class SFXPlayer : MonoBehaviour
 {
     public AudioSource source;
-    public bool coroutinePlaying;
+    public SoundState soundState;
+    public float timePassed;
+    public bool isLoop;
 
     public void PlaySFX(AudioClip sfx)
     {
@@ -15,27 +24,77 @@ public class SFXPlayer : MonoBehaviour
     public Coroutine sfxCoroutine;
     private IEnumerator PlaySFXCoroutine(AudioClip sfx)
     {
+        soundState = SoundState.Playing;
         SFXSystem.singleton.sfxPlayerPool.Remove(this);
         SFXSystem.singleton.activeSFXPlayers.Add(this);
 
         source.clip = sfx;
         source.Play();
 
-        float timePassed = 0;
+        timePassed = 0;
         while (timePassed < sfx.length)
         {
             timePassed += Time.deltaTime;
             yield return null;
         }
 
-        SFXSystem.singleton.sfxPlayerPool.Add(this);
-        SFXSystem.singleton.activeSFXPlayers.Remove(this);
+        CleanUp();
+    }
+
+    public void PlaySFXOnLoop(AudioClip sfx)
+    {
+        isLoop = true;
+        soundState = SoundState.Playing;
+        SFXSystem.singleton.sfxPlayerPool.Remove(this);
+        SFXSystem.singleton.activeSFXPlayers.Add(this);
+
+        source.clip = sfx;
+        source.loop = true;
+        source.Play();
+    }
+
+    public void PauseSFX()
+    {
+        if (soundState != SoundState.Playing) return;
+
+        soundState = SoundState.Paused;
+        source.Pause();
+    }
+
+    public void UnpauseSFX()
+    {
+        sfxCoroutine = StartCoroutine(ResumeSFXCoroutine());
+    }
+
+    private IEnumerator ResumeSFXCoroutine()
+    {
+        if (soundState != SoundState.Paused) yield break;
+
+        soundState = SoundState.Playing;
+        source.UnPause();
+
+        if (isLoop) yield break;
+
+        while (timePassed < source.clip.length)
+        {
+            timePassed += Time.deltaTime;
+            yield return null;
+        }
+
+        CleanUp();
     }
 
     public void CleanUp()
     {
+        if (soundState == SoundState.Finished) return;
+
         source.Stop();
-        StopCoroutine(sfxCoroutine);
+        source.loop = false;
+        source.clip = null;
+        soundState = SoundState.Finished;
+        timePassed = 0;
+        isLoop = false;
+        if (sfxCoroutine != null) StopCoroutine(sfxCoroutine);
 
         if (!SFXSystem.singleton.sfxPlayerPool.Contains(this)) SFXSystem.singleton.sfxPlayerPool.Add(this);
         if (SFXSystem.singleton.activeSFXPlayers.Contains(this)) SFXSystem.singleton.activeSFXPlayers.Remove(this);
--- SFX System/SFXSystem.cs
+++ SFX System/SFXSystem.cs
@@ -24,8 +24,17 @@ public class SFXSystem : MonoBehaviour
     public const string path = "SFX";
     public Dictionary<string, AudioClip> sfxDict = new Dictionary<string, AudioClip>();
 
+    [Header("SFX Players")]
     public List<SFXPlayer> sfxPlayerPool;
     public List<SFXPlayer> activeSFXPlayers;
+    public List<SFXPlayer> pausedSFXPlayers;
+
+    [Header("SFX ID Lists")]
+    public List<string> UIOnHoverSFX;
+    public List<string> UIOnSubmitSFX;
+    public List<string> UIOnCancelSFX;
+    public List<string> FallingWhistleSFX;
+
 
     /// <summary>
     /// Adds all SFX in "Resources/SFX" to sfxDict
@@ -45,7 +54,7 @@ public class SFXSystem : MonoBehaviour
     /// Gets SFXPlayer from pool and plays a SFX
     /// </summary>
     /// <param name="sfxID"></param>
-    public void PlaySFX(string sfxID)
+    public void PlaySFX(string sfxID, bool isLoop)
     {
         AudioClip sfx;
         sfxDict.TryGetValue(sfxID, out sfx);
@@ -61,7 +70,49 @@ public class SFXSystem : MonoBehaviour
             return;
         }
 
+        if (isLoop) sfxPlayerPool[0].PlaySFXOnLoop(sfx);
+        else sfxPlayerPool[0].PlaySFX(sfx);
+    }
+
+    public void PlayRandomSFX(List<string> sfxIDs)
+    {
+        AudioClip sfx;
+        int randIndex = Random.Range(0, sfxIDs.Count);
+        sfxDict.TryGetValue(sfxIDs[randIndex], out sfx);
+
+        if (sfx == null)
+        {
+            Debug.LogError("SFX " + sfxIDs[randIndex] + " Not Found");
+            return;
+        }
+        if (sfxPlayerPool.Count == 0)
+        {
+            Debug.LogError("No Available SFX Players");
+            return;
+        }
+
         sfxPlayerPool[0].PlaySFX(sfx);
+    }
+
+
+    public void PauseAllSFX()
+    {
+        for (int i = 0; i < activeSFXPlayers.Count; i++)
+        {
+            activeSFXPlayers[i].PauseSFX();
+            pausedSFXPlayers.Add(activeSFXPlayers[i]);
+            activeSFXPlayers.Remove(activeSFXPlayers[i]);
+        }
+    }
+
+    public void UnpauseAllSFX()
+    {
+        for (int i = 0; i < pausedSFXPlayers.Count; i++)
+        {
+            pausedSFXPlayers[i].UnpauseSFX();
+            activeSFXPlayers.Add(pausedSFXPlayers[i]);
+            pausedSFXPlayers.Remove(pausedSFXPlayers[i]);
+        }
     }
 
     /// <summary>
